b106c06141154a55f7c7d7c03f387b499cf608a3
ac149483 <ac149483@8CG8163KY4>
2022-05-11 12:46:25 +1200

    Add project files.

---
+++ Excercise6A/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("This program will ask you to enter a number, to which the program will tell you whether that number is positive, negative or zero.");
+
+int num;
+
+Console.Write("Enter a number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 0)
+{
+    Console.WriteLine("This number is zero");
+}
+else if (num > 0)
+{
+    Console.WriteLine("This number is positive");
+}
+else
+{
+    Console.WriteLine("This number is negative");
+}
